4b5ddefd818e82001de4d31698c92796b76429bd
zinleo <zin_leo@163.com>
2018-09-20 10:40:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# React-framework
+
+  基于 Create-react-app、React 16.5.2的React 项目骨架
+
+## 功能
+
+1、支持预处理器 scss
+
+## 启动
+
+1、克隆项目
+  
+  git clone git@xxx.com/git
+
+2、安装依赖
+
+  npm install | yarn
+
+3、启动
+
+  npm start
+
+4、打包
+
+  npm run build
+
+## 建议
+
+1、建议组件名采用 首字母大写 + 驼峰 命名法
---
+++ src/views/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <Footer></Footer>
+      </div>
+    );
+  }
+}
+
+export default Home;
